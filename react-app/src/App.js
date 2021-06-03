c923d9330dae647cd01de52be4a301f03212c08e
Mitchell Lin <ml887@cornell.edu>
2021-06-03 16:25:11 -0400

    Simple Visualization Test

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import rd3 from 'react-d3-library';
+import Example from './Visualization.js';
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Example />
         <p>
           Mitchell's React App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
