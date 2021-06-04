9b11387b893133ecf27bd7b344b3ac905883e7cd
Mitchell Lin <ml887@cornell.edu>
2021-06-04 14:22:49 -0400

    getting started

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import rd3 from 'react-d3-library';
-import Example from './Visualization.js';
+import Sunburst from './Visualization.js';
 
 
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Example />
-        <p>
-          Mitchell's React App
-        </p>
-      </header>
-    </div>
+    <Sunburst />
   );
 }
 
--- react-app/src/Visualization.js
+++ react-app/src/Visualization.js
@@ -1,30 +1,62 @@
 import React, {Component} from 'react';
 import * as d3 from "d3";
+import { svg } from 'd3';
 
 class Visualization extends Component {
     componentDidMount() {
-      this.drawVisualization();
+        this.drawVisualization();
     }
+
       
     drawVisualization() {
-      
-      const svg = d3.select("body")
-      .append("svg")
-      .attr("width", 100)
-      .attr("height", 100)
-                    
-      svg.append("rect")
-        .attr("x", 25)
-        .attr("y", 25)
-        .attr("width", 50)
-        .attr("height", 50)
-        .attr("fill", "green")
+
+        d3.json("exampleDataset.json").then( data => {
+
+            var currentDepth = 0; 
+
+            console.log(data);
+            const root = d3.hierarchy(data)
+                .sum(d => d.value)
+                .sort((a, b) => b.value - a.value);
+            console.log(root);
+            const partition = d3.partition().size([2 * Math.PI, root.height + 1])(root)
+
+            root.each(d => d.target = {
+                
+                length: 1,
+                
+            });
+            // root.each(d => d.target = {
+            //     x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+            //     x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+            //     y0: Math.max(0, d.y0 - p.depth),
+            //     y1: Math.max(0, d.y1 - p.depth)
+            // });
+
+        });
+
+        
+
+        const svg = d3.select("body")
+        .append("svg")
+        .attr("width", 700)
+        .attr("height", 700)
+        .style("border", "2px solid lightgrey")
+        .style("margin", "50px")
+
+        
+                        
+        svg.append("rect")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", 600)
+            .attr("height", 600)
+            .attr("fill", "green")
+
     }
           
     render(){
-        return <svg ref={node => this.node = node}
-        width={500} height={500}>
-        </svg>
+        return svg;
     }
   }
       
